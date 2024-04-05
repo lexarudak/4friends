5ac04f0e6cc8241a5017c7badfa0739aa62f259a
Aliaksei Rudak <arudak@ebsco.com>
2024-04-05 15:58:17 +0500

    1

--- src/pages/home-page/home-page.tsx
+++ src/pages/home-page/home-page.tsx
@@ -1,12 +1,30 @@
 // import { NextMatches } from "../../components/next-matches/next-matches";
+// import { useSelector } from "react-redux";
 import { NextMatches } from "../../components/next-matches/next-matches";
 import { Table } from "../../components/table/table";
 import { Timer } from "../../components/timer/timer";
 import styles from "./home-page.module.scss";
+// import { nextMatchesSelector } from "../../store/next-matches/next-matches.selector";
 
 export const HomePage = (): JSX.Element => {
+  // const nextMatches = useSelector(nextMatchesSelector);
+  // const fetchData = async () => {
+  //   const ans = await fetch(
+  //     "http://176.57.70.40:8080/rest4friends/recievedata.cfc?method=receiveData",
+  //     {
+  //       method: "POST",
+  //       headers: {
+  //         "Content-Type": "application/json",
+  //       },
+  //       body: JSON.stringify(nextMatches),
+  //     },
+  //   );
+  //   const data = await ans.json();
+  //   console.log({ data });
+  // };
   return (
     <section className={styles.page}>
+      {/* <button onClick={fetchData}>TEST</button> */}
       <Timer className={styles.timer} />
       <NextMatches />
       <Table />
