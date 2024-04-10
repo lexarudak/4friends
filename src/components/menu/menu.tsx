ab01952aadad329a14236eee811d4e16bae6996f
Aliaksei Rudak <lexarudak2@gmail.com>
2024-04-10 19:50:28 +0500

    1

--- src/components/menu/menu.tsx
+++ src/components/menu/menu.tsx
@@ -5,9 +5,14 @@ import { isMenuOpenSelector } from "../../store/app/app.selector";
 import { closeMenu } from "../../store/app/app.slice";
 import { Nav } from "../nav/nav";
 import { CSSTransition } from "react-transition-group";
+import Logo from "../../../public/euro-logo.png";
+import { Link } from "react-router-dom";
+import { ROUTE_LIST } from "../../router/route-list";
+import { BREAKPOINTS, useBreakPoint } from "../../hooks";
 
 export const Menu = (): JSX.Element => {
   const isOpen = useSelector(isMenuOpenSelector);
+  const BP = useBreakPoint();
 
   const cn = {
     [styles.menu]: true,
@@ -22,6 +27,11 @@ export const Menu = (): JSX.Element => {
   return (
     <>
       <aside className={classNames(cn)}>
+        {BP === BREAKPOINTS.xl && (
+          <Link to={ROUTE_LIST.home} className={styles.logo}>
+            <img alt="logo" src={Logo} />
+          </Link>
+        )}
         <Nav />
       </aside>
       <CSSTransition in={isOpen} timeout={200} classNames="fade" unmountOnExit>
